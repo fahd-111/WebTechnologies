642519dc11a7c07e5c3cfc2999ef1b8ec36884af
fahd-111 <fahad.jcat123@gmail.com>
2024-03-12 00:59:41 +0500

    mew file

--- TermProject/ScrollAnimation.js
+++ TermProject/ScrollAnimation.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imgLayout = document.querySelector('.img-layout');
     const img1 = document.querySelector('.img1');
     const img2 = document.querySelector('.img2');
     const img3 = document.querySelector('.img3');
+    const heroText = document.querySelector('.hero-text'); // Get the hero-text element
 
     const handleScroll = () => {
         const scrollDistance = (window.pageYOffset || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - window.innerHeight) * 100;
 
-        // Apply a different percentage of the scroll distance for each image
-        // Note: These values are illustrative. Adjust them according to your desired effect
-        imgLayout.style.transform = `translate3d(0px, ${-scrollDistance }%, 0px)`;
-        img1.style.transform = `translate3d(0px, ${-scrollDistance * 0.4}%, 0px)`;
-        img2.style.transform = `translate3d(0px, ${-scrollDistance * 0.1}%, 0px)`;
+        img1.style.transform = `translate3d(0px, ${-scrollDistance * 0.8}%, 0px)`;
+        img2.style.transform = `translate3d(0px, ${-scrollDistance * 0.3}%, 0px)`;
         img3.style.transform = `translate3d(0px, ${-scrollDistance * 0.2}%, 0px)`;
+        heroText.style.transform = `translate(-50%, ${-50 - scrollDistance * 0.8}%)`; // Adjust -0.1 as needed to control speed of movement
+        // Decrease opacity as scrolling progresses
+        heroText.style.opacity = 1 - scrollDistance * 0.009; // Adjust 0.01 to control rate of opacity reduction
+
+
     };
 
     window.addEventListener('scroll', handleScroll);
